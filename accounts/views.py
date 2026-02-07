6e603b1b117b97a5f2a4bdb0ddd258c5e3c93804
Jai <jairevankar98@gmail.com>
2026-02-08 02:01:12 +0530

    Views Created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from accounts.serializers import LoginSerializer, RegisterSerializer, UserSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, req):
+        serializer = RegisterSerializer(data=req.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            user_data = UserSerializer(user).data
+            return Response(user_data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    def post(self, req):
+        serializer = LoginSerializer(data=req.data)
+
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
